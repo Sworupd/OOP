0366991bbc8dea8ce89f83a155c5ad8b742d0aac
LENOVO <sworupdhakal88@gmail.com>
2024-08-13 13:43:11 +0545

    j

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import models.Mammalia;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Mammalia cow = new Mammalia();
+        cow.getBye();
+        cow.getHello();
+
+        Mammalia dog = new Mammalia();
+        dog.getBye();
+    }
+}
---
+++ src/models/Mammalia.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Mammalia {
+    // properties
+    public int noofchambers = 4;
+    public boolean hairybody = true;
+
+    // methods
+    public void getHello(){
+        System.out.println("Hello There!");
+    }
+    public void getBye(){
+        System.out.println("Bye There!");
+    }
+}
